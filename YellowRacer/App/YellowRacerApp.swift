9dbc3f7f777fa0edc1f407c478bf02f23104be20
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-11-19 11:32:09 +0200

    init

---
+++ YellowRacer/App/YellowRacerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YellowRacerApp.swift
+//  YellowRacer
+//
+//  Created by Serhii Babchuk on 19.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct YellowRacerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            YRLaunchView()
+        }
+    }
+}
